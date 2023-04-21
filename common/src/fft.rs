a9e5707df30ef521fb979f94942553423492fb52
swasilyev <swasilyev@gmail.com>
2023-04-21 16:33:26 +0200

    fft impl cleaned up

--- common/src/fft.rs
+++ common/src/fft.rs
@@ -4,56 +4,94 @@ use ark_poly::univariate::DensePolynomial;
 use num_bigint::BigUint;
 use num_integer::Integer;
 
-struct CooleyTukeyDomain<F: PrimeField> {
+trait FftDomain<F: Field> {
+    fn fft(&self, coeffs: &[F]) -> Vec<F>;
+    fn n(&self) -> usize;
+    fn w(&self) -> F;
+}
+
+struct NaiveDomain<F: Field> {
     n: usize,
-    n1: usize,
-    n2: usize,
-    g1: F,
-    g2: F,
+    w: F,
+}
+
+impl<F: Field> NaiveDomain<F> {
+    fn new(w: F, n: usize) -> Self {
+        assert!(w.pow([n as u64]).is_one());
+        Self {
+            n,
+            w,
+        }
+    }
+}
+
+impl<F: Field> FftDomain<F> for NaiveDomain<F> {
+    fn fft(&self, coeffs: &[F]) -> Vec<F> {
+        assert_eq!(coeffs.len(), self.n);
+        let poly = DensePolynomial::from_coefficients_slice(coeffs);
+        let mut res = vec![F::zero(); self.n];
+        res[0] = poly.evaluate(&F::one());
+        res[1] = poly.evaluate(&self.w);
+        let mut wi = self.w;
+        for i in 2..self.n {
+            wi *= self.w;
+            res[i] = poly.evaluate(&wi);
+        }
+        res
+    }
+
+    fn n(&self) -> usize {
+        self.n
+    }
+
+    fn w(&self) -> F {
+        self.w
+    }
+}
+
+struct CooleyTukeyDomain<F: Field, D: FftDomain<F>> {
+    n: usize,
+    w: F,
+    d1: D,
+    d2: D,
     twiddles: Vec<Vec<F>>,
 }
 
-impl<F: PrimeField> CooleyTukeyDomain<F> {
-    fn new(g: F, n1: usize, n2: usize) -> Self {
-        let n = n1 * n2;
-        assert!(g.pow([n as u64]).is_one());
-        let g2 = g.pow([n1 as u64]);
-        let g1 = g.pow([n2 as u64]);
-        assert!(g1.pow([n1 as u64]).is_one());
-        assert!(g2.pow([n2 as u64]).is_one());
-
-        let mut twiddles = vec![vec![F::zero(); n1]; n2];
-        for i2 in 0..n2 {
-            let mut inner = vec![F::zero(); n1];
-            for k1 in 0..n1 {
-                inner[k1] = g.pow([(k1 * i2) as u64]);
+impl<F: Field, D: FftDomain<F>> CooleyTukeyDomain<F, D> {
+    fn new(w: F, d1: D, d2: D) -> Self {
+        let n = d1.n() * d2.n();
+        assert!(w.pow([n as u64]).is_one());
+        let mut twiddles = vec![vec![F::zero(); d1.n()]; d2.n()];
+        for i2 in 0..d2.n() {
+            let mut inner = vec![F::zero(); d1.n()];
+            for k1 in 0..d1.n() {
+                inner[k1] = w.pow([(k1 * i2) as u64]);
             }
             twiddles[i2] = inner;
         }
 
         Self {
             n,
-            n1,
-            n2,
-            g1,
-            g2,
+            w,
+            d1,
+            d2,
             twiddles,
         }
     }
+}
 
+impl<F: Field, D: FftDomain<F>> FftDomain<F> for CooleyTukeyDomain<F, D> {
     fn fft(&self, coeffs: &[F]) -> Vec<F> {
         let n = self.n;
-        let n1 = self.n1;
-        let n2 = self.n2;
-        let wn1 = self.g2;
-        let wn2 = self.g1;
+        let n1 = self.d1.n();
+        let n2 = self.d2.n();
         assert_eq!(coeffs.len(), n);
 
         let mut inner_dfts = vec![vec![F::zero(); n2]; n1];
         for k1 in 0..n1 {
             let inner_coeffs: Vec<_> = coeffs.iter().cloned().skip(k1).step_by(n1).collect();
             assert_eq!(inner_coeffs.len(), n2);
-            inner_dfts[k1] = dft(&wn1, &inner_coeffs);
+            inner_dfts[k1] = self.d2.fft(&inner_coeffs);
         }
 
         let mut res = vec![F::zero(); n];
@@ -62,27 +100,21 @@ impl<F: PrimeField> CooleyTukeyDomain<F> {
             for k1 in 0..n1 {
                 outer_coeffs[k1] = self.twiddles[i2][k1] * inner_dfts[k1][i2];
             }
-            let outer_dft = dft(&wn2, &outer_coeffs);
+            let outer_dft = self.d1.fft(&outer_coeffs);
             for i1 in 0..n1 {
                 res[i1 * n2 + i2] = outer_dft[i1];
             }
         }
         res
     }
-}
 
-fn dft<F: Field>(w: &F, coeffs: &[F]) -> Vec<F> {
-    let n = coeffs.len();
-    let poly = DensePolynomial::from_coefficients_slice(coeffs);
-    let mut res = vec![F::zero(); n];
-    res[0] = poly.evaluate(&F::one());
-    res[1] = poly.evaluate(w);
-    let mut wi = *w;
-    for i in 2..n {
-        wi *= w;
-        res[i] = poly.evaluate(&wi);
+    fn n(&self) -> usize {
+        self.n
     }
-    res
+
+    fn w(&self) -> F {
+        self.w
+    }
 }
 
 fn gen<F: PrimeField>(n: usize) -> F {
@@ -97,7 +129,7 @@ fn gen<F: PrimeField>(n: usize) -> F {
 
 #[cfg(test)]
 mod tests {
-    use ark_std::{test_rng, UniformRand};
+    use ark_std::{end_timer, start_timer, test_rng, UniformRand};
 
     use super::*;
 
@@ -110,12 +142,23 @@ mod tests {
         let n2 = 47;
         let n = n1 * n2;
 
-        let g = gen::<Fq>(n);
+        let w = gen::<Fq>(n);
+        let w1 = w.pow([n2 as u64]);
+        let w2 = w.pow([n1 as u64]);
+        let d1 = NaiveDomain::new(w1, n1);
+        let d2 = NaiveDomain::new(w2, n2);
+        let ctd = CooleyTukeyDomain::new(w, d1, d2);
+        let nd = NaiveDomain::new(w, n);
 
-        let domain = CooleyTukeyDomain::new(g, n1, n2);
-        let x: Vec<_> = (0..n).map(|_| Fq::rand(rng)).collect();
-        let fft = domain.fft(&x);
-        let dft = dft(&g, &x);
+        let coeffs: Vec<_> = (0..n).map(|_| Fq::rand(rng)).collect();
+
+        let t_fft = start_timer!(|| "fft");
+        let fft = ctd.fft(&coeffs);
+        end_timer!(t_fft);
+
+        let t_dft = start_timer!(|| "dft");
+        let dft = nd.fft(&coeffs);
+        end_timer!(t_dft);
         assert_eq!(fft, dft);
     }
 }
