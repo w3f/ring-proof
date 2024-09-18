cfa60768f2236c886615f25259d7ecd60b18cb78
Davide Galassi <davxy@datawok.net>
2024-09-18 12:57:09 +0200

    Blanks

--- ring/src/piop/params.rs
+++ ring/src/piop/params.rs
@@ -32,7 +32,7 @@ pub struct PiopParams<F: PrimeField, Curve: SWCurveConfig<BaseField=F>> {
 
 impl<F: PrimeField, Curve: SWCurveConfig<BaseField=F>> PiopParams<F, Curve> {
     pub fn setup(domain: Domain<F>, h: Affine<Curve>, seed: Affine<Curve>) -> Self {
-        let padding_point = crate::hash_to_curve(b"w3f/ring-proof/common/padding");       
+        let padding_point = crate::hash_to_curve(b"w3f/ring-proof/common/padding");
         let scalar_bitlen = Curve::ScalarField::MODULUS_BIT_SIZE as usize;
         // 1 accounts for the last cells of the points and bits columns that remain unconstrained
         let keyset_part_size = domain.capacity - scalar_bitlen - 1;
