644dc5d012ac949726dc05fa937e4859c481be46
Davide Galassi <davxy@datawok.net>
2024-09-18 15:53:21 +0200

    Nit

--- ring/src/lib.rs
+++ ring/src/lib.rs
@@ -35,7 +35,7 @@ pub fn find_complement_point<Curve: SWCurveConfig>() -> Affine<Curve> {
     }
 }
 
-// Hash to curve using TAI
+// Try and increment hash to curve.
 pub(crate) fn hash_to_curve<F: PrimeField, Curve: SWCurveConfig<BaseField = F>>(message: &[u8]) -> Affine<Curve> {
     use blake2::Digest;
     let mut seed = message.to_vec();
