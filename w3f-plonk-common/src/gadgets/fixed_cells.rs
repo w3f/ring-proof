86c6b8ceac053c103ac47981566f387869f6fda2
Sergey Vasilyev <swasilyev@gmail.com>
2025-03-19 03:31:13 +0300

    FixedCells gadget refactored

--- w3f-plonk-common/src/gadgets/fixed_cells.rs
+++ w3f-plonk-common/src/gadgets/fixed_cells.rs
@@ -10,8 +10,6 @@ use crate::{const_evals, Column, FieldColumn};
 
 pub struct FixedCells<F: FftField> {
     col: Rc<FieldColumn<F>>,
-    col_first: F,
-    col_last: F,
     l_first: FieldColumn<F>,
     l_last: FieldColumn<F>,
 }
@@ -27,40 +25,49 @@ pub struct FixedCellsValues<F: Field> {
 impl<F: FftField> FixedCells<F> {
     pub fn init(col: Rc<FieldColumn<F>>, domain: &Domain<F>) -> Self {
         assert_eq!(col.len, domain.capacity);
-        let col_first = col.evals.evals[0];
-        let col_last = col.evals.evals[domain.capacity - 1];
         let l_first = domain.l_first.clone();
         let l_last = domain.l_last.clone();
         Self {
             col,
-            col_first,
-            col_last,
             l_first,
             l_last,
         }
     }
 
     pub fn constraints(&self) -> Vec<Evaluations<F>> {
-        let col = &self.col;
-        let domain = col.domain_4x();
-        let first = &const_evals(self.col_first, domain);
-        let last = &const_evals(self.col_last, domain);
-        let col = &self.col.evals_4x;
-        let l_first = &self.l_first.evals_4x;
-        let l_last = &self.l_last.evals_4x;
-        let c = &(l_first * &(col - first)) + &(l_last * &(col - last));
+        let domain_capacity = self.col.len; // that's an ugly way to learn the capacity, but we've asserted it above.
+        let c = &Self::constraint_cell(&self.col, &self.l_first, 0)
+            + &Self::constraint_cell(&self.col, &self.l_last, domain_capacity - 1);
         vec![c]
     }
 
     pub fn constraints_linearized(&self, _z: &F) -> Vec<DensePolynomial<F>> {
         vec![DensePolynomial::zero()]
     }
+
+    /// Constraints the column `col` to have the value `col[i]` at index `i`.
+    /// `li` should be the `i-th` Lagrange basis polynomial `li = L_i(X)`.
+    /// The constraint polynomial is `c(X) = L_i(X).col(X) - col[i].L_i(X)`.
+    pub fn constraint_cell(col: &FieldColumn<F>, li: &FieldColumn<F>, i: usize) -> Evaluations<F> {
+        let cell_val = col.evals[i];
+        let domain = col.domain_4x();
+        let cell_val = &const_evals(cell_val, domain);
+        let col = &col.evals_4x;
+        let li = &li.evals_4x;
+        li * &(col - cell_val)
+    }
+}
+
+impl<F: Field> FixedCellsValues<F> {
+    pub fn evaluate_for_cell(col_eval: F, li_eval: F, cell_val: F) -> F {
+        li_eval * (col_eval - cell_val)
+    }
 }
 
 impl<F: Field> VerifierGadget<F> for FixedCellsValues<F> {
     fn evaluate_constraints_main(&self) -> Vec<F> {
-        let c =
-            (self.col - self.col_first) * self.l_first + (self.col - self.col_last) * self.l_last;
+        let c = Self::evaluate_for_cell(self.col, self.l_first, self.col_first)
+            + Self::evaluate_for_cell(self.col, self.l_last, self.col_last);
         vec![c]
     }
 }
