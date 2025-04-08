22ff1740905418fd7d42fb603303b8a7bf8cf5dc
Sergey Vasilyev <swasilyev@gmail.com>
2025-04-08 21:57:16 +0200

    test comes after build

--- evm-vrfier/src/lib.rs
+++ evm-vrfier/src/lib.rs
@@ -1,13 +1,12 @@
-alloy::sol!(
-    #[allow(missing_docs)]
-    #[sol(rpc)]
-    Counter,
-    "contracts/out/Counter.sol/Counter.json"
-);
-
 #[cfg(test)]
 mod tests {
     use alloy::primitives::Uint;
+
+    alloy::sol!(
+        #[sol(rpc)]
+        Counter,
+        "contracts/out/Counter.sol/Counter.json"
+    );
 
     #[tokio::test]
     async fn it_works() -> Result<(), Box<dyn std::error::Error>> {
@@ -15,7 +14,7 @@ mod tests {
             .with_recommended_fillers()
             .on_anvil_with_wallet();
 
-        let counter = crate::Counter::deploy(&provider).await?;
+        let counter = crate::tests::Counter::deploy(&provider).await?;
 
         let number = counter.number().call().await?._0;
         let _ = counter.increment().send().await?;
