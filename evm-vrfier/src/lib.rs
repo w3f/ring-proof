84a145785226d1b242be07cf03202d711df6fa03
Sergey Vasilyev <swasilyev@gmail.com>
2025-04-08 13:45:00 +0200

    a rust test

--- evm-vrfier/src/lib.rs
+++ evm-vrfier/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+alloy::sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    Counter,
+    "contracts/out/Counter.sol/Counter.json"
+);
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use alloy::primitives::Uint;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+        let provider = alloy::providers::builder()
+            .with_recommended_fillers()
+            .on_anvil_with_wallet();
+
+        let counter = crate::Counter::deploy(&provider).await?;
+
+        let number = counter.number().call().await?._0;
+        let _ = counter.increment().send().await?;
+        assert_eq!(counter.number().call().await?._0, number + Uint::from(1));
+
+        Ok(())
     }
 }
