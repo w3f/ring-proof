a5514dc2cbaa9ad81a7c96d551769e7c3520986c
Sergey Vasilyev <swasilyev@gmail.com>
2025-04-09 21:56:47 +0200

    kzg test in solidity - 2

--- evm-vrfier/contracts/test/PlonkKzg.t.sol
+++ evm-vrfier/contracts/test/PlonkKzg.t.sol
@@ -5,7 +5,7 @@ import "../src/SoladyBls.sol";
 import "../src/BlsGenerators.sol";
 import "../src/PlonkKzg.sol";
 
-contract SoladyBlsTest is Test {
+contract PlonkKzgTest is Test {
     PlonkKzg kzg;
     BLS.G1Point[] srs_g1;
     BLS.G2Point tau_g2;
@@ -42,7 +42,7 @@ contract SoladyBlsTest is Test {
         proof = commit(q);
     }
 
-    function eval(bytes32[] memory coeffs, uint256 z) internal view returns (uint256 v) {
+    function eval(bytes32[] memory coeffs, uint256 z) internal pure returns (uint256 v) {
         uint256 l = coeffs.length;
         v = uint256(coeffs[l - 1]);
         for (uint256 j = l - 1; j > 0; j--) {
@@ -52,6 +52,7 @@ contract SoladyBlsTest is Test {
 
     function verify_single(BLS.G1Point memory c, uint256 z, uint256 v, BLS.G1Point memory proof)
         internal
+        view
         returns (bool)
     {
         BLS.G1Point[] memory msm_bases = new BLS.G1Point[](2);
@@ -78,7 +79,7 @@ contract SoladyBlsTest is Test {
         assertEq(res[2], bytes32(uint256(0)));
     }
 
-    function test_kzg() public {
+    function test_kzg() public view {
         bytes32[] memory poly = new bytes32[](3);
         for (uint256 i = 0; i < poly.length; i++) {
             poly[i] = bytes32(i + 10);
@@ -88,5 +89,37 @@ contract SoladyBlsTest is Test {
         BLS.G1Point memory proof = prove(poly, z);
         uint256 v = eval(poly, z);
         assert(verify_single(c, z, v, proof));
+    }
+
+    function test_plonk_kzg() public view {
+        bytes32[] memory poly = new bytes32[](3);
+        for (uint256 i = 0; i < poly.length; i++) {
+            poly[i] = bytes32(i + 10);
+        }
+        BLS.G1Point memory c = commit(poly);
+        uint256 z1 = 666;
+        uint256 z2 = 777;
+        BLS.G1Point memory proof_z1 = prove(poly, z1);
+        BLS.G1Point memory proof_z2 = prove(poly, z2);
+        uint256 eval_at_z1 = eval(poly, z1);
+        uint256 eval_at_z2 = eval(poly, z2);
+
+        assert(verify_single(c, z1, eval_at_z1, proof_z1));
+        assert(verify_single(c, z2, eval_at_z2, proof_z2));
+
+        BLS.G1Point[] memory polys_z1 = new BLS.G1Point[](1);
+        uint256[] memory evals_at_z1 = new uint256[](1);
+        polys_z1[0] = c;
+        evals_at_z1[0] = eval_at_z1;
+
+        BLS.G1Point memory poly_z2 = c;
+
+        bytes32 nu = bytes32(uint256(123));
+        uint256 r = 345;
+        bytes32[] memory nus = new bytes32[](1);
+        nus[0] = nu;
+        proof_z1 = BlsGenerators.g1_mul(proof_z1, nu);
+
+        assert(kzg.verify_plonk_kzg(polys_z1, poly_z2, z1, z2, evals_at_z1, eval_at_z2, proof_z1, proof_z2, nus, r));
     }
 }
